2d711cf3a8a6b297a52fec6f346541d60cdfded0
sechiyo97 <sechiyo97@gmail.com>
2024-04-14 02:08:30 +0900

    긱 웨이트 필터링 추가

--- lib/boardgames/board_games_screen.dart
+++ lib/boardgames/board_games_screen.dart
@@ -16,11 +16,24 @@ class BoardGamesScreen extends StatefulWidget {
 class _BoardGamesScreenState extends State<BoardGamesScreen> {
   late Future<List<BoardGame>> boardGames;
   final TextEditingController _searchNameController = TextEditingController();
-  final TextEditingController _searchPlayerCountController = TextEditingController();
-  final TextEditingController _searchBestForController = TextEditingController();
+  final TextEditingController _searchPlayerCountController =
+      TextEditingController();
+  final TextEditingController _searchBestForController =
+      TextEditingController();
   String _selectedSearchCategory = '전체';
-  List<String> categories = ['전체', '전략', '추상전략', '파티', '패밀리', '퍼즐', '테마틱'];
+  List<String> categories = [
+    '전체',
+    '전략',
+    '추상전략',
+    '파티',
+    '패밀리',
+    '퍼즐',
+    '테마틱',
+    '어린이'
+  ];
   bool _showSearchOptions = false; // 검색 옵션 표시 상태
+  List<String> geekWeightCategories = ['전체', '1점대', '2점대', '3점대', '4점대'];
+  String _selectedGeekWeight = '전체'; // Default to show all
 
   @override
   void initState() {
@@ -29,15 +42,30 @@ class _BoardGamesScreenState extends State<BoardGamesScreen> {
   }
 
   Future<List<BoardGame>> loadBoardGameCsvData() async {
-    final csvDataString = await rootBundle.loadString('assets/csv/daynyong-house-boardgames.csv');
-    List<List<dynamic>> csvList = const CsvToListConverter().convert(csvDataString);
+    final csvDataString =
+        await rootBundle.loadString('assets/csv/daynyong-house-boardgames.csv');
+    List<List<dynamic>> csvList =
+        const CsvToListConverter().convert(csvDataString);
     return csvList.sublist(1).map((row) => BoardGame.fromCsvRow(row)).toList();
   }
 
   List<BoardGame> filterData(List<BoardGame> data) {
     return data.where((game) {
-      bool matchesName = _searchNameController.text.isEmpty || game.name.toLowerCase().contains(_searchNameController.text.toLowerCase());
-      bool matchesCategory = _selectedSearchCategory == '전체' || game.category == _selectedSearchCategory;
+      bool matchesGeekWeight = true;
+      if (_selectedGeekWeight != '전체') {
+        double gameGeekWeight = double.tryParse(game.geekWeight) ?? 0;
+        int weightFloor =
+            int.parse(_selectedGeekWeight[0]); // Assumes format like '1점대'
+        matchesGeekWeight =
+            (gameGeekWeight >= weightFloor && gameGeekWeight < weightFloor + 1);
+      }
+
+      bool matchesName = _searchNameController.text.isEmpty ||
+          game.name
+              .toLowerCase()
+              .contains(_searchNameController.text.toLowerCase());
+      bool matchesCategory = _selectedSearchCategory == '전체' ||
+          game.category == _selectedSearchCategory;
       bool matchesPlayerCount = true;
       bool matchesBestFor = true;
 
@@ -51,11 +79,13 @@ class _BoardGamesScreenState extends State<BoardGamesScreen> {
             var parts = game.playerCount.split('~').map(int.tryParse).toList();
             // 범위의 시작과 끝을 확인
             if (parts[0] != null && parts[1] != null) {
-              matchesPlayerCount = searchCount >= parts[0]! && searchCount <= parts[1]!;
+              matchesPlayerCount =
+                  searchCount >= parts[0]! && searchCount <= parts[1]!;
             }
           } else {
             // game.playerCount가 단일 숫자인 경우
-            matchesPlayerCount = game.playerCount == _searchPlayerCountController.text;
+            matchesPlayerCount =
+                game.playerCount == _searchPlayerCountController.text;
           }
         }
       }
@@ -70,7 +100,8 @@ class _BoardGamesScreenState extends State<BoardGamesScreen> {
             var parts = game.bestFor.split('~').map(int.tryParse).toList();
             // 범위의 시작과 끝을 확인
             if (parts[0] != null && parts[1] != null) {
-              matchesBestFor = searchCount >= parts[0]! && searchCount <= parts[1]!;
+              matchesBestFor =
+                  searchCount >= parts[0]! && searchCount <= parts[1]!;
             }
           } else {
             // game.playerCount가 단일 숫자인 경우
@@ -79,7 +110,11 @@ class _BoardGamesScreenState extends State<BoardGamesScreen> {
         }
       }
 
-      return matchesName && matchesCategory && matchesPlayerCount && matchesBestFor;
+      return matchesName &&
+          matchesCategory &&
+          matchesPlayerCount &&
+          matchesBestFor &&
+          matchesGeekWeight;
     }).toList();
   }
 
@@ -103,64 +138,100 @@ class _BoardGamesScreenState extends State<BoardGamesScreen> {
       ),
       body: Column(
         children: [
-          if (_showSearchOptions) Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: DropdownButton<String>(
-              value: _selectedSearchCategory,
-              onChanged: (value) {
-                if (value != null) {
-                  setState(() {
-                    _selectedSearchCategory = value;
-                  });
-                }
-              },
-              items: categories.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
-          if (_showSearchOptions) Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchNameController,
-              decoration: const InputDecoration(
-                labelText: '이름으로 검색',
-                suffixIcon: Icon(Icons.search),
+          if (_showSearchOptions)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: [
+                  const Text("게임 종류: "),
+                  DropdownButton<String>(
+                    value: _selectedSearchCategory,
+                    onChanged: (value) {
+                      if (value != null) {
+                        setState(() {
+                          _selectedSearchCategory = value;
+                        });
+                      }
+                    },
+                    items: categories
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
               ),
-              onChanged: (value) => setState(() {}),
             ),
-          ),
-          if (_showSearchOptions) Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchPlayerCountController,
-              decoration: const InputDecoration(
-                labelText: '플레이 인원으로 검색',
-                suffixIcon: Icon(Icons.people),
+          if (_showSearchOptions)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  const Text("긱 웨이트: "),
+                  DropdownButton<String>(
+                    value: _selectedGeekWeight,
+                    onChanged: (value) {
+                      if (value != null) {
+                        setState(() {
+                          _selectedGeekWeight = value;
+                        });
+                      }
+                    },
+                    items: geekWeightCategories
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ],
               ),
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {});
-              },
             ),
-          ),
-          if (_showSearchOptions) Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchBestForController,
-              decoration: const InputDecoration(
-                labelText: '베스트 인원으로 검색',
-                suffixIcon: Icon(Icons.people),
+          if (_showSearchOptions)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchNameController,
+                decoration: const InputDecoration(
+                  labelText: '이름으로 검색',
+                  suffixIcon: Icon(Icons.search),
+                ),
+                onChanged: (value) => setState(() {}),
               ),
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {});
-              },
             ),
-          ),
+          if (_showSearchOptions)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchPlayerCountController,
+                decoration: const InputDecoration(
+                  labelText: '플레이 인원으로 검색',
+                  suffixIcon: Icon(Icons.people),
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {});
+                },
+              ),
+            ),
+          if (_showSearchOptions)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchBestForController,
+                decoration: const InputDecoration(
+                  labelText: '베스트 인원으로 검색',
+                  suffixIcon: Icon(Icons.people),
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {});
+                },
+              ),
+            ),
           Expanded(
             child: FutureBuilder<List<BoardGame>>(
               future: boardGames,
